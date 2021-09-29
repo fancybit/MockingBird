ae9f348030d4e3fdb5561b546c3584a0b80dbc64
fancybit <fancybit@qq.com>
2021-09-29 15:41:15 +0800

    修改默认配置文件

--- web/config/default.py
+++ web/config/default.py
@@ -1,6 +1,6 @@
 AUDIO_SAMPLES_DIR = 'samples\\'
 DEVICE = '0'
-HOST = 'localhost'
+HOST = '0.0.0.0'
 PORT = 8080
 MAX_CONTENT_PATH =1024 * 1024 * 4  # mp3文件大小限定不能超过4M
 SECRET_KEY = "mockingbird_key"
